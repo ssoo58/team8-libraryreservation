1c76747ed1745c50a35d09c6941d3aba94d27821
ssoo58 <zinna2002@gmail.com>
2026-06-03 23:15:25 +0900

    Create SeatController.java

---
+++ src/controller/SeatController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import model.ReservationState;
+import repository.SeatRepository;
+import view.MainView;
+import view.SeatView;
+
+public class SeatController {
+    private SeatView view;
+    private String userName;
+
+    public SeatController(SeatView view, String userName) {
+        this.view = view;
+        this.userName = userName;
+        initListeners();
+        loadSeats();
+    }
+
+    private void initListeners() {
+        view.getReserveButton().addActionListener(e -> reserve());
+        view.getBackButton().addActionListener(e -> goBack());
+    }
+
+    private void loadSeats() {
+        SeatRepository.getInstance().findAll().forEach(seat -> {
+            switch (seat.getStatus()) {
+                case RESERVED:
+                    view.setSeatReserved(seat.getSeatNumber());
+                    break;
+                case IN_USE:
+                    view.setSeatInUse(seat.getSeatNumber());
+                    break;
+                default:
+                    view.setSeatAvailable(seat.getSeatNumber());
+                    break;
+            }
+        });
+    }
+
+    private void reserve() {
+        if (ReservationState.hasReservationFor(userName)) {
+            view.showErrorMessage("이미 예약된 좌석이 있습니다.");
+            return;
+        }
+
+        int seatNumber = view.getSelectedSeatNumber();
+        if (seatNumber == 0) {
+            view.showErrorMessage("좌석을 먼저 선택하세요.");
+            return;
+        }
+
+        ReservationState.reserve(userName, seatNumber);
+        view.setSeatReserved(seatNumber);
+        view.showSuccessMessage(seatNumber + "번 좌석이 예약되었습니다.\n10분 이내에 입실완료해주세요.");
+    }
+
+    private void goBack() {
+        view.dispose();
+        new MainView(userName);
+    }
+}
