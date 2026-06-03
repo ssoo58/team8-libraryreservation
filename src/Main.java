0db93171f82009f379b9ac6fa64d30e0c1fe028b
ssoo58 <zinna2002@gmail.com>
2026-06-03 23:23:56 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,9 @@
+import view.LoginView;
+import controller.LoginController;
 
+public class Main {
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        new LoginController(loginView);
+    }
+}
